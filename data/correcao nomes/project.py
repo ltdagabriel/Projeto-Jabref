d15e6522b97489e8c744b9b252743ac600a51c1e
caionakai <kaioesperto@hotmail.com>
2018-10-31 11:12:59 -0300

    correção dos nomes + gerando csv

--- data/correcao nomes/project.py
+++ data/correcao nomes/project.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import csv
 from more_itertools import *
 from symspellpy.symspellpy import SymSpell, Verbosity  # import the module
 
@@ -12,20 +14,26 @@ def main():
 						 prefix_length)
 	# load dictionary
 
-	dictionary_path = os.path.join("C:\\Users\\kaioe\\OneDrive\\Documentos",
+	dictionary_path = os.path.join(sys.argv[0],
 								   "dict_final.txt")
 	count_index = 1  # column of the term frequency in the dictionary text file
 	term_index = 0  # column of the term in the dictionary text file
 	if not sym_spell.load_dictionary(dictionary_path, term_index, count_index):
 		print("Dictionary file not found")
 		return
-
-	with open("nomesErrados.txt", "r",  encoding="utf-8") as f:
+	list = []
+	lista_nome_errado = []
+	
+	#xd = pd.read_csv('Null_names.csv')
+	
+	with open("Null_names.csv", "r") as f:
 		line = f.readline()
 		while line:
+			line = f.readline()
 			# p = peekable(f.readlines())
 			# print(p.peek())
 			print("Nome pra ser corrigido:", line)
+			lista_nome_errado.append(line)
 			# lookup suggestions for multi-word input strings (supports compound
 			# splitting & merging)
 			input_term = (line)	    # max edit distance per lookup (per single word, not per whole input string)
@@ -33,10 +41,18 @@ def main():
 			suggestions = sym_spell.lookup_compound(input_term,
 													max_edit_distance_lookup)
 			# display suggestion term, edit distance, and term frequency
+			#writer = csv.writer(f, delimiter='\t') 
 			for suggestion in suggestions:
+				#writer.writerow(['']+[suggestion.term])
+				list.append(suggestion.term)
 				print("{}, {}, {}".format("sugestao: ",suggestion.term, suggestion.count,
 										  suggestion.distance))
-			line = f.readline()
+			
 
+	with open("output.csv", "w", newline='' ) as fcs:
+		escreve = csv.writer(fcs, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		escreve.writerow(['Nome entrada','Nome corrigido'])
+		for l in zip(list,lista_nome_errado):
+			escreve.writerow([l[1], l[0]])
 if __name__ == "__main__":
 	main()
